61055d1aa966ec3faefff4e04714e57b3b2c0a15
pablo <pabrober@gmail.com>
2020-04-07 03:37:58 -0600

    main class

--- exambple/src/main/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/AssemblyApp/problem/AssemblyApp.java
+++ exambple/src/main/java/gt/edu/umg/ingenieria/sistemas/ingsoftware/AssemblyApp/problem/AssemblyApp.java
@@ -7,18 +7,12 @@ import java.util.*;
  */
 public class AssemblyApp {
 
-    /**
-     * Default constructor
-     */
-    public AssemblyApp() {
-    }
+    
 
 
-    /**
-     * @param args
-     */
-    public void main(String[] args) {
-        // TODO implement here
+    
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
     }
 
 }
